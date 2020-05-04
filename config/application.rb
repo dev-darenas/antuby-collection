3fafae45790744243b1ef38ee9c725361bf297a0
Daniel Arenas <darenas@softdremas.co>
2020-05-04 18:34:38 -0500

    added localitation

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,11 @@ module Antuby
     config.load_defaults 6.0
 
     config.autoload_paths << Rails.root.join('lib')
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :es
   end
 end
