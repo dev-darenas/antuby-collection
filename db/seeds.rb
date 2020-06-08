c5ff5ea0be067c62174a7d834cbc4f2bb84acf91
Daniel Arenas <darenas@softdremas.co>
2020-06-08 11:01:26 -0500

    Added Seed config

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,7 @@
 Role.find_or_create_by(name: :admin, name_es: 'Administración')
 Role.find_or_create_by(name: :collection_advisor, name_es: 'Asesor de cobranza')
 Role.find_or_create_by(name: :debt_collector, name_es: 'Recaudador')
+
+## Super Admins ##
+Admin.create(email: 'darenas@softdreams.co', password: '123456789')
+Admin.create(email: 'jquinones@softdreams.co', password: '123456789')
