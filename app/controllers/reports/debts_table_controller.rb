0cec5a39aead15e10087276bb5b50fc1b84b2c63
Duvan Hernandez <duvanherfi@gmail.com>
2020-06-17 14:35:01 -0500

    the filter is improved to include the advisor

--- app/controllers/reports/debts_table_controller.rb
+++ app/controllers/reports/debts_table_controller.rb
@@ -1,15 +1,18 @@
 module Reports
   class DebtsTableController < EnterpriseController
-    
+
     def index
       @start_date = params[:start_date] || Date.today.beginning_of_month
       @end_date   = params[:end_date] || Date.today.end_of_month
+      @collector_id = params[:collector_advisor_id]
 
       @invoices = @enterprise
                     .invoices
                     .active
                     .includes(:third, :activities)
                     .where(payment_date: @start_date..@end_date)
+
+      @invoices = @invoices.where(collector_advisor_id: @collector_id ) if @collector_id.present?
     end
   end
 end
